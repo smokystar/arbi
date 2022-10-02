f9e66ce3859480e53d687aa86614f3b592f969a7
smokystar <101525606+smokystar@users.noreply.github.com>
2022-10-03 00:36:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ async def on_startup(_):
     print('Bot is online')
 
 import commands
-import clients
+
 
 commands.register_handlers_client_partners(dp)
-# clients.register_handlers(dp)
+
 
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
