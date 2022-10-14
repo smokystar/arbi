ee8bdf6fe0f7caa5bc259f471c0fe3e861374cfc
smokystar <101525606+smokystar@users.noreply.github.com>
2022-10-14 23:43:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from aiogram.utils import executor
 from create import dp
 import psycopg2
-
-con = psycopg2.connect(user="uhwgxkaboaglce",
-                                password="75db761e1367ffdf929f791edc4dcd1a58936cbe3fa9e87c920ca16338f2374c",
-                                host="ec2-52-48-159-67.eu-west-1.compute.amazonaws.com",
-                                port="5432",
-                                database="d5ehlrsmpq329l")
-cur = con.cursor()
+from po import con, cur
 # cur.execute(f'create table best_profit_procents(scheme_id bigint primary key, time_now text, procent float, a_pay text, b_pay text)')
 # con.commit()
 # cur.execute(f'create table arbi_users(tele_id bigint primary key, date text, pp_bot int, bc_bot int, exc_bot int, fastbuy_bot int)')
