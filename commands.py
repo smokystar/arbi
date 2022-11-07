7c09078563be17614423aadb71fd9075de70f2d0
smokystar <101525606+smokystar@users.noreply.github.com>
2022-11-07 18:30:40 +0300

    Update commands.py

--- commands.py
+++ commands.py
@@ -40,7 +40,7 @@ async def scan(message: types.Message):
     user_id = message.from_user.id
     while n < 10:
         try:
-            await scheme_analizer(1.05, admin_id)
+            await scheme_analizer(1.075, admin_id)
             time.sleep(60)
         except Exception as e:
             await bot.send_message(message.from_user.id, f'Error occured!\n\n {e}')
